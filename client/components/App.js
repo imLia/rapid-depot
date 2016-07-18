32ff190d823f2c001049ce647abc9c468f58dcfc
Barry Lavides <lavidesbarry@gmail.com>
2016-07-18 11:49:10 +0800

    Render React main component

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello from App component</div>;
+  }
+}
